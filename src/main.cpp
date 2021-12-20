d39642640ff5d351eff5c8281eec4f0b0502d37d
Igor Katkov <ikatkov@gmail.com>
2021-12-20 00:04:53 -0800

    working code

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,56 @@
 #include "SoftwareSerial.h"
 #include <EasyButton.h>
 #include <EEPROM.h>
-#include "Countimer.h"
+
+//#define serialON
 
 static const byte SLEEP_INTERVAL = 40;
 static const byte CAL_BUTTON_PIN = 0; //pin 5
 static const byte LED_PIN1 = 2;       //pin 7
 
-//TODO change to LED_PIN2 = 3;
-static const byte LED_PIN2 = 2;   //pin 2
+#ifdef serialON
+static const byte LED_PIN2 = 2; //fake pint
+#else
+static const byte LED_PIN2 = 3;   //pin 2
+#endif
+
 static const byte LED_PIN3 = 4;   //pin 3
 static const byte MOSFET_PIN = 1; //pin 6
 
+
+static const byte ON = 0; 
+static const byte OFF = 1;
+byte state = ON;
+
+#ifdef serialON
 static const byte TX_PIN = 3; //Pin 1
 SoftwareSerial MySerial(-1, TX_PIN);
+#endif
 
 EasyButton calButton(CAL_BUTTON_PIN);
-Countimer t24cycle;
-Countimer tTurnOff;
-byte onTime = 1;
+byte onTime = 0;
+unsigned long turnOnTime;
+unsigned long turnOffTime;
 
 void onTurnOff()
 {
+    #ifdef serialON
+    MySerial.print(millis());
+    MySerial.print(F("\t"));
     MySerial.println(F("ontTurnOff"));
+    #endif
     digitalWrite(MOSFET_PIN, LOW);
     digitalWrite(LED_PIN1, LOW);
     digitalWrite(LED_PIN2, LOW);
     digitalWrite(LED_PIN3, LOW);
+    state = OFF;
 }
 
 void configureTurnOffDelay()
 {
+    #ifdef serialON
     MySerial.println(F("configureTurnOffDelay"));
+    #endif
     digitalWrite(LED_PIN1, LOW);
     digitalWrite(LED_PIN2, LOW);
     digitalWrite(LED_PIN3, LOW);
@@ -40,53 +59,63 @@ void configureTurnOffDelay()
     {
     case 0:
     {
+        #ifdef serialON
         MySerial.println(F("case 0"));
-        tTurnOff.setCounter(0, 0, 15, CountType::COUNT_DOWN, onTurnOff);
+        #endif
+        turnOffTime = millis() + (unsigned long)15 * 1000;
         digitalWrite(LED_PIN1, HIGH);
         break;
     }
     case 1:
     {
+        #ifdef serialON
         MySerial.println(F("case 1"));
-        tTurnOff.setCounter(0, 0, 30, CountType::COUNT_DOWN, onTurnOff);
+        #endif
+        turnOffTime = millis() + (unsigned long)30 * 1000;
         digitalWrite(LED_PIN1, HIGH);
         digitalWrite(LED_PIN2, HIGH);
         break;
     }
     case 2:
     {
+        #ifdef serialON
         MySerial.println(F("case 2"));
-        tTurnOff.setCounter(0, 0, 45, CountType::COUNT_DOWN, onTurnOff);
+        #endif
+        turnOffTime = millis() + (unsigned long)45 * 1000;
         digitalWrite(LED_PIN1, HIGH);
         digitalWrite(LED_PIN2, HIGH);
         digitalWrite(LED_PIN3, HIGH);
         break;
     }
     }
-    tTurnOff.restart();
 }
 
 void onNewCycle()
 {
+    #ifdef serialON
+    MySerial.print(millis());
+    MySerial.print(F("\t"));
     MySerial.println(F("onNewCycle"));
+    #endif
     digitalWrite(MOSFET_PIN, HIGH);
-    t24cycle.restart();
+    turnOnTime = millis() + (unsigned long)60*1000;
     configureTurnOffDelay();
+    state = ON;
 }
 
 void onCalButtonPressed()
 {
+    #ifdef serialON
+    MySerial.print(millis());
+    MySerial.print(F("\t"));
     MySerial.println(F("onCalButtonPressed"));
-    if (tTurnOff.isCounterRunning())
+    #endif
+    if (state == ON)
     {
         onTime = (onTime + 1) % 3;
-        EEPROM.write(0, onTime);
-        configureTurnOffDelay();
+        EEPROM.write(1, onTime);
     }
-    else
-    {
-        onNewCycle();
-    }
+    onNewCycle();
 }
 
 void setup()
@@ -95,9 +124,11 @@ void setup()
     // see http://becomingmaker.com/tuning-attiny-oscillator/
     OSCCAL -= 3;
 
+    #ifdef serialON
     pinMode(TX_PIN, OUTPUT);
     MySerial.begin(9600);
     MySerial.println(F("setup..."));
+    #endif
 
     pinMode(LED_PIN1, OUTPUT);
     pinMode(LED_PIN2, OUTPUT);
@@ -115,7 +146,7 @@ void setup()
     digitalWrite(LED_PIN2, LOW);
     digitalWrite(LED_PIN3, LOW);
     //read from eeprom here
-    byte onTime = EEPROM.read(0);
+    byte onTime = EEPROM.read(1);
     if (onTime < 0 || onTime > 2)
     {
         onTime = 0;
@@ -123,14 +154,16 @@ void setup()
     configureTurnOffDelay();
     calButton.begin();
     calButton.onPressed(onCalButtonPressed);
-
-    t24cycle.setCounter(0, 1, 0, CountType::COUNT_DOWN, onNewCycle);
     onNewCycle();
 }
 
 void loop()
 {
-    t24cycle.run();
-    tTurnOff.run();
+    if(state == ON && millis() > turnOffTime){
+        onTurnOff();
+    }
+    if(millis() > turnOnTime){
+        onNewCycle();
+    }
     calButton.read();
 }
